9046ee781c181797a6206974093d33f76d9491b8
hero <hero@asterisk-works.jp>
2016-05-02 07:13:21 +0900

    add the disolay of results

--- src/code.js
+++ src/code.js
@@ -29,7 +29,7 @@ function showSidebar(){
  *  @return 緯度、経度の配列。リクエストに失敗した場合には文字列 'ERROR' を返す
  */
 function geocode(address){
-  if (!address||address == ''){return ''}
+  if (!address||address == ''){return [['','']]}
   var url = "http://newspat.csis.u-tokyo.ac.jp/cgi-bin/simple_geocode.cgi?addr=" + address;
   try {
     var res = UrlFetchApp.fetch(url)
@@ -50,9 +50,11 @@ function geocode(address){
  * 選択したセル(複数のカラムを選択していれば左端のカラム)に記載されている住所で
  * 緯度経度を求め、出力カラムに出力する。
  * @param outCol [integer] 緯度を書き込むカラムのインデックス。経度はoutCol+1のカラムに書き込む
+ * @return [Object] 変換結果{addr:入力住所数,skip:スキップ数(出力カラムが空でなかった),fail:変換失敗数}
  */
 function batchGeocode(outCol){
   var outNumCols = 2 //出力のカラム幅。緯度と経度で2カラム
+  var result = {addr:0,skip:0,fail:0}
 
   //選択されているレンジから住所を取得し、変換する
   var addrRange = reselectRange();
@@ -68,16 +70,22 @@ function batchGeocode(outCol){
   var d = data(outRangeVals,outRangeFoms)
   var outVals = []
   for(var i = 0; i < values.length; i++){
+    var address = values[i][0]
+    if(!!address||address != ''){result.addr++}
     var code = d(i); // 出力セルの値を一旦格納
     if (code == null){ //出力セルが空の時だけ処理
-      code = geocode(values[i][0]) //レンジの左端の列をループ
-      if(!Array.isArray(code)){code = ['','']}else{code = code[0]}
-    }
+      code = geocode(address) //レンジの左端の列をループ
+      if(!Array.isArray(code)){
+        code = ['','']
+        result.fail++
+      }else{ code = code[0] }
+    } else { if(!!address||address != ''){result.skip++}}
     outVals.push(code)
   }
   outRange.setValues(outVals)
 
   SpreadsheetApp.getActiveSpreadsheet().toast("処理が完了しました。", '完了', 5);
+  return result;
 }
 
 /*
